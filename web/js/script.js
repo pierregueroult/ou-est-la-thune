37e1b0b038a1e2c3aba9941be873bb6fcf3a5887
Nicolas Conguisti <nicolas.conguisti1@gmail.com>
2026-01-11 20:32:52 +0100

    feat : Adding DAB infos on click on marker

--- web/js/script.js
+++ web/js/script.js
@@ -20,6 +20,23 @@ function distanceMeters([lat1, lng1], [lat2, lng2]) {
   return R * distance;
 }
 
+function addEventOnPoint(feature, layer) {
+  layer.on("click", () => {
+    const p = feature.properties;
+    if (p.name != null) console.log("Distributeur " + p.name);
+    if (p.brand != null) console.log("Banque " + p.brand);
+    if (p.type != null) console.log("Type " + p.type);
+    if (p.operator != null) console.log("Opérateur : " + p.operator);
+    if (p.wheelchair != null) console.log("Accessible aux personnes à mobilité réduite : " + p.wheelchair);
+    if (p.opening_hours != null) console.log("Ouverte de " + p.opening_hours);
+    if (p.meta_name_com != null && p.meta_name_dep != null && p.meta_name_reg != null)
+      console.log("Située " + p.meta_name_com + " / " + p.meta_name_dep + " / " + p.meta_name_reg);
+    if (p.meta_osm_url != null) console.log("Lien OpenStreetMap : " + p.meta_osm_url);
+  });
+}
+
+
+
 window.onload = async () => {
   let layer = L.tileLayer(
     "https://{s}.basemaps.cartocdn.com/light_all/{z}/{x}/{y}.png",
@@ -41,6 +58,7 @@ window.onload = async () => {
       const distance = distanceMeters(userPosition, [lat, lng]);
       return distance <= radiusMeters;
     },
+    onEachFeature: addEventOnPoint
   });
 
   let circle = L.circle(userPosition, {
@@ -49,17 +67,15 @@ window.onload = async () => {
     fillOpacity: 0.1,
   });
 
-  // Création de l'icône rouge pour le marker
+  // Icon for user
   const icon = new L.Icon({
-  iconUrl: '../assets/images/marker-icon-2x-red.png',
-  shadowUrl: '../assets/images/marker-shadow.png',
-  iconSize: [25, 41],
-  iconAnchor: [12, 41],
-  popupAnchor: [1, -34],
-  shadowSize: [41, 41]
-});
-
-  // Création du marker utilisateur avec l'icône rouge
+    iconUrl: '../assets/images/marker-icon-2x-red.png',
+    shadowUrl: '../assets/images/marker-shadow.png',
+    iconSize: [25, 41],
+    iconAnchor: [12, 41],
+    popupAnchor: [1, -34],
+    shadowSize: [41, 41]
+  });
   let userMarker = L.marker(userPosition, {
     icon: icon,
   });
@@ -71,6 +87,9 @@ window.onload = async () => {
     attributonControl: false,
     layers: [layer, geoLayer, circle, userMarker],
   });
+
+  // Click on one of the markers
+
 
   // Sharing the position and website
   const shareButton = document.getElementById("share");
