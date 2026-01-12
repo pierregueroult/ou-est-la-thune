2160d1838cab725907a3616abf2f185b553efd62
Pierre Guéroult <pierregueroult2022@gmail.com>
2026-01-12 17:16:55 +0100

    feat(script): edit transform to take several files in input

--- scripts/transform.js
+++ scripts/transform.js
@@ -2,15 +2,22 @@ const fs = require("node:fs/promises");
 const path = require("node:path");
 
 async function transformInputToSource(
-  inputFileName, // nom de l'entrée
+  inputFileName, // nom de l'entrée (peut être un tableau)
   outputFileName, // nom de la sortie
   transformFunction, // fonction de traitement
 ) {
-  const inputPath = path.join(__dirname, "sources", inputFileName);
+  const inputs = Array.isArray(inputFileName) ? inputFileName : [inputFileName];
+
+  const contents = await Promise.all(
+    inputs.map((file) =>
+      fs.readFile(path.join(__dirname, "sources", file), "utf8"),
+    ),
+  );
+
   const outputPath = path.join(__dirname, "results", outputFileName);
 
-  const content = await fs.readFile(inputPath, "utf8");
-  const transformedContent = transformFunction(content);
+  // On passe le contenu de chaque fichier comme un argument séparé à la fonction de transformation
+  const transformedContent = transformFunction(...contents);
   await fs.writeFile(outputPath, transformedContent);
 }
 
